3aaff036d4490dcec50f7c902a459b1042a144f5
Ben Villiere <benoit.villiere@gmail.com>
2019-09-04 03:04:45 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React, { useContext, createContext } from 'react'
+import { injectStripe } from 'react-stripe-elements'
+
+const Context = createContext()
+
+export const useStripe = () => useContext(Context)
+
+const HookProvider = ({ children, stripe }) => {
+    return <Context.Provider value={stripe}>{children}</Context.Provider>
+}
+
+export const StripeHookProvider = injectStripe(HookProvider)
